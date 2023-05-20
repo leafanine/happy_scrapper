5681c7b4ce9f7ab94cff5ea4416208f8fcf05578
Leafa Pendragon <43307991+leafanine@users.noreply.github.com>
2023-05-21 02:43:19 +0530

    separate csv for each class

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+from urllib.parse import urljoin
 
-URL = "https://timetable.gift.edu.in/tt.php?c=1&b=52&sem=4&st=S&s=70"
-page = requests.get(URL)
+URL= "https://timetable.gift.edu.in/"
+page= requests.get(URL)
 
-#parsing it
-soup = BeautifulSoup(page.content, 'html5lib')
-filename = "test.csv"
-f=open(filename, 'w')
-csv_writer = csv.writer(f)
+#parse it
+soup = BeautifulSoup(page.content, 'html.parser')
 
-for tr in soup.find_all('tr'):
-  data = []
+links = []
 
-  for th in tr.find_all('th'):
-    data.append(th.text)
+urls = soup.find_all('a')
 
-  for td in tr.find_all('td'):
-    data.append(td.text)
+for link in urls:
+    hyper = link.get('href')
+    full_url = urljoin(URL, hyper)
+    #print(full_url)
+    links.append(full_url)
 
-  if data:
-    print(" {}".format(','.join(data)))
-    csv_writer.writerow(data)
+j=5
 
-f.close()
+for i in links[5:]:
+    time_table = requests.get(i)
+    bsoup = BeautifulSoup(time_table.content, 'html5lib')
+    
+    filename = f"test_{j}.csv"
+    f=open(filename, 'w')
+    csv_writer = csv.writer(f)
+    
+    for tr in bsoup.find_all('tr'):
+        data=[]
+        
+        for th in tr.find_all('th'):
+            data.append(th.text)
+            
+        for td in tr.find_all('td'):
+            data.append(td.text)
+            
+        if data:
+            #uncomment this line if you want to print it whole in terminal
+            ##print(" {}",format(','.join(data)))
+            csv_writer.writerow(data)
+            
+    f.close()
+    j=j+1
